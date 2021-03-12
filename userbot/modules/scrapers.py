1a8c69e43a8a9f3c9a294045d797cb12f3f6129f
THEALIFHAKER1 <56091627+THEALIFHAKER1@users.noreply.github.com>
2021-03-12 11:34:37 +0800

    Clean up

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module containing various scrapers. """
-
-import os
 import time
 import asyncio
 import shutil
